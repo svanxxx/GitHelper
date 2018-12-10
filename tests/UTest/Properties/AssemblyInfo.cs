f4a159a5dcdb6cc7e82bd59afff818c5c02bea52
svan <6204931@gmail.com>
2018-12-10 23:19:58 +0300

    unit tests added

---
+++ tests/UTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("148ed869-093a-42fa-aa18-4bec232fffa3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tests/UTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using GitHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UTest
+{
+	[TestClass]
+	public class GitTest
+	{
+		[TestMethod]
+		public void TestBranchTT()
+		{
+			Branch b = new Branch(null);
+			b.NAME = "TT";
+			if (b.TTID != -1)
+			{
+				Assert.Fail("invalid process of branch text");
+			}
+			b.NAME = "TT1";
+			if (b.TTID != 1)
+			{
+				Assert.Fail("invalid process of branch text");
+			}
+			b.NAME = "TT123";
+			if (b.TTID != 123)
+			{
+				Assert.Fail("invalid process of branch text");
+			}
+			b.NAME = "TT123hiall";
+			if (b.TTID != 123)
+			{
+				Assert.Fail("invalid process of branch text");
+			}
+			b.NAME = "TT123_hiall123455";
+			if (b.TTID != 123)
+			{
+				Assert.Fail("invalid process of branch text");
+			}
+		}
+	}
+}
